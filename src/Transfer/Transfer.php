eb0d3ba0854b90386ca3e72dd0e126938cb406aa
Bright Antwi Boasiako <brightantwiboasiako@aol.com>
2021-03-12 14:07:36 +0000

    Reading reason from description for failed transfers.

--- src/Transfer/Transfer.php
+++ src/Transfer/Transfer.php
@@ -2,6 +2,7 @@
 
 namespace TheTeller\Transfer;
 
+use Exception;
 use TheTeller\Support\Http\HttpClientInterface;
 
 abstract class Transfer{
@@ -9,6 +10,7 @@ abstract class Transfer{
 
     const THETELLER_TRANSFER_ENDPOINT = 'v1.1/transaction/process';
     const THETELLER_TRANSFER_STATUS_SUCCESSFUL = 'successful';
+    const THETELLER_TRANSFER_STATUS_FAILED = 'failed';
     const THETELLER_TRANSFER_TYPE_MOBILE_MONEY = 'mobile-money';
     const THETELLER_TRANSFER_TYPE_BANK = 'bank';
 
@@ -42,14 +44,23 @@ abstract class Transfer{
      * Gets the transfer reason
      *
      * @return string|null
-     * @throws \Exception
+     * @throws Exception
      */
     public function getReason()
     {
-        if(!isset($this->transferResponse['reason']))
-            throw new \Exception('Transfer must be processed first.');
+        if(empty($this->transferResponse['status']))
+            throw new Exception('Transfer must be processed first.');
 
-        return $this->transferResponse['reason'];
+        if($this->transferResponse['status'] == self::THETELLER_TRANSFER_STATUS_SUCCESSFUL)
+            return $this->transferResponse['reason'];
+
+        else if($this->transferResponse['status'] == self::THETELLER_TRANSFER_STATUS_FAILED
+            && isset($this->transferResponse['description'])){
+            // Transfer failed.
+            return $this->transferResponse['description'];
+        }
+
+        return 'Unknown reason.';
     }
 
 }
